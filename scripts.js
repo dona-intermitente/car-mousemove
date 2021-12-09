9b217d46c93994183c6ebe638ec14009537e55a2
sindi <sindidaconceicao@gmail.com>
2021-12-09 11:42:48 -0400

    wheels rotation

--- scripts.js
+++ scripts.js
@@ -4,19 +4,19 @@ import { GLTFLoader } from './three.js-master/examples/jsm/loaders/GLTFLoader.js
 import { RoomEnvironment } from './three.js-master/examples/jsm/environments/RoomEnvironment.js';
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(render);
 renderer.outputEncoding = THREE.sRGBEncoding;
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.toneMappingExposure = 0.85;
 document.body.appendChild(renderer.domElement);
 
-const pmremGenerator = new THREE.PMREMGenerator( renderer );
+const pmremGenerator = new THREE.PMREMGenerator(renderer);
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffffff);
-scene.fog = new THREE.Fog( 0xeeeeee, 10, 50 );
-scene.environment = pmremGenerator.fromScene( new RoomEnvironment() ).texture;
+scene.fog = new THREE.Fog(0xeeeeee, 10, 50);
+scene.environment = pmremGenerator.fromScene(new RoomEnvironment()).texture;
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 200);
 camera.position.set(-5, 5, 5);
@@ -25,28 +25,38 @@ camera.rotation.y = - Math.PI / 4;
 camera.rotation.x = Math.atan(- 1 / Math.sqrt(2));
 
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
 
-new GLTFLoader().load('car.gltf', function (gltf) {
+const wheels = [];
+
+new GLTFLoader().load('car.glb', function (gltf) {
     const model = gltf.scene;
+
+    wheels.push(
+        model.getObjectByName('wheel_fl'),
+        model.getObjectByName('wheel_fr'),
+        model.getObjectByName('wheel_bl'),
+        model.getObjectByName('wheel_br')
+    );
+
     scene.add(model);
 });
 
+window.onresize = function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+};
+
 function render() {
+    const time = performance.now() / 1000;
+
+    for (let i = 0; i < wheels.length; i++) {
+        wheels[i].rotation.x = time * Math.PI;
+    }
+
     renderer.render(scene, camera);
-    requestAnimationFrame(render);
-    controls.update();
 }
 
 render();
-
-window.onresize = function () {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(width, height);
-
-    render();
-};
